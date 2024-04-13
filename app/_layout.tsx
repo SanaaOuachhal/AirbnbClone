5a319fdba594c94ef36cd8ea253c40d88acac384
khaoualaND <khaoula.nadi2@gmail.com>
2024-04-13 11:03:16 +0000

    authentification

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,6 +5,29 @@ import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import { TouchableOpacity } from 'react-native';
+import { get } from 'react-native/Libraries/TurboModule/TurboModuleRegistry';
+import * as SecureStore from 'expo-secure-store';
+import { ClerkProvider, useAuth } from '@clerk/clerk-expo';
+
+//load clerk's key
+const CLERK_PUBLISHABLE_KEY = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
+//expo secure storage package (create our own token)
+const tokenCache = {
+  async getToken(key: string){
+    try{
+      return SecureStore.getItemAsync(key);
+    }catch(err){
+      return null;
+    }
+  },
+  async saveToken(key: string, value:string){
+    try{
+      return SecureStore.setItemAsync(key, value);
+    }catch(err){
+      return;
+    }
+  },
+}
 
 //import { useColorScheme } from '@/components/useColorScheme';
 
@@ -45,11 +68,22 @@ export default function RootLayout() {
     return null;
   }
 
-  return <RootLayoutNav />;
+  return (
+  <ClerkProvider publishableKey={CLERK_PUBLISHABLE_KEY!} tokenCache={tokenCache}>
+    <RootLayoutNav />
+  </ClerkProvider>
+  );
 }
 
 function RootLayoutNav() {
   const router = useRouter();
+  const {isLoaded, isSignedIn}=useAuth();
+
+  useEffect(()=>{
+    if(isLoaded && !isSignedIn){
+      router.push('/(modals)/login')
+    }
+  },[isLoaded]);
   return (
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
