9cbf79cec61d5d3b48b1990be90de96af5cd8a7f
khaoualaND <khaoula.nadi2@gmail.com>
2024-05-01 13:14:17 +0100

    try the changes

--- components/Listings.tsx
+++ components/Listings.tsx
@@ -1,5 +1,7 @@
-import { View, Text } from 'react-native'
-import React, { useEffect } from 'react'
+import { View, Text, FlatList, ListRenderItem } from 'react-native'
+import React, { useEffect, useRef, useState } from 'react'
+import { defaultStyles } from '@/constants/Styles';
+import { Link } from 'expo-router';
 
 interface Props {
     listings: any[];
@@ -7,15 +9,28 @@ interface Props {
     category: string;
   }
 
-const Listings = ({ listings, category}: Props) => {
-    useEffect(() =>{
-        console.log('RELOAD LISTINGS: ',listings.length)
-    }, [category]);
-  return (
-    <View>
-      <Text>Listings</Text>
-    </View>
-  )
-}
+const Listings = ({ listings : items, category}: Props) => {
+    const[loading, setLoading] = useState(false);
+    const listRef = useRef<FlatList>(null);
 
-export default Listings
+    useEffect(() =>{
+        console.log('RELOAD LISTINGS: ',items.length)
+        setLoading(true);
+
+        setTimeout(()=>{
+          setLoading(false);
+        },200);
+    }, [category]);
+
+    const renderRow: ListRenderItem<any> = ({ item }) => (
+      <Link href={`/listing/${item.id}`}>Go there</Link>
+    );
+
+  return (
+    <View style={defaultStyles.container}>
+      <FlatList renderItem={renderRow} ref={listRef} data={loading ? [] : items} />
+    </View>
+  );
+};
+
+export default Listings;
