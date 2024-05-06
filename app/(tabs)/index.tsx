4c068b9ede2751cae2f468ed71871011e1ac060e
khaoualaND <khaoula.nadi2@gmail.com>
2024-05-06 19:33:05 +0100

    Bottom Sheet is finally working

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,6 @@
 import { View } from 'react-native';
 import React, { useMemo, useState } from 'react';
-//import ListingsBottomSheet from '@/components/ListingsBottomSheet';
+import {GestureHandlerRootView} from 'react-native-gesture-handler' ;
 import listingsData from '@/assets/data/airbnb-listings.json';
 import ListingsMap from '@/components/ListingsMap';
 import listingsDataGeo from '@/assets/data/airbnb-listings.geo.json';
@@ -31,15 +31,17 @@ import ListingsBottomSheet from '@/components/ListingsBottomSheet';
 
 
   return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
     <View style={{flex: 1, marginTop: 80}}>
       <Stack.Screen
        options={{
           header: () => <ExploreHeader onCategoryChanged={onDataChanged}/>,
       }}/>
-      {/*<Listings listings={items} category={category} />*/ }
+     {/* <Listings listings={items} category={category} />*/}
       <ListingsMap listings={getoItems} />
       <ListingsBottomSheet listings={items} category={category}/>
     </View>
+    </GestureHandlerRootView>
   )
 }
 
